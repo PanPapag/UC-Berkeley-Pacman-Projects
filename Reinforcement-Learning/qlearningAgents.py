203f4f234220a0447f9901f9dcb9e2f6761cec94
PanPapag <pan_papageorgiou@hotmail.com>
2019-04-18 21:36:03 +0300

    Q-Learning completed

--- Reinforcement-Learning/qlearningAgents.py
+++ Reinforcement-Learning/qlearningAgents.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -43,6 +43,7 @@ class QLearningAgent(ReinforcementAgent):
         ReinforcementAgent.__init__(self, **args)
 
         "*** YOUR CODE HERE ***"
+        self.values = util.Counter()
 
     def getQValue(self, state, action):
         """
@@ -51,7 +52,7 @@ class QLearningAgent(ReinforcementAgent):
           or the Q node value otherwise
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.values[(state, action)]
 
 
     def computeValueFromQValues(self, state):
@@ -62,7 +63,12 @@ class QLearningAgent(ReinforcementAgent):
           terminal state, you should return a value of 0.0.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        maxQ = float("-inf")
+        for action in self.getLegalActions(state):
+            maxQ = max(maxQ,self.getQValue(state,action))
+
+        return maxQ if maxQ != float('-inf') else 0.0
+
 
     def computeActionFromQValues(self, state):
         """
@@ -71,7 +77,11 @@ class QLearningAgent(ReinforcementAgent):
           you should return None.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        policies = util.Counter()
+        for action in self.getLegalActions(state):
+            policies[action] = self.getQValue(state,action)
+
+        return policies.argMax()
 
     def getAction(self, state):
         """
@@ -88,7 +98,7 @@ class QLearningAgent(ReinforcementAgent):
         legalActions = self.getLegalActions(state)
         action = None
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+
 
         return action
 
@@ -102,7 +112,10 @@ class QLearningAgent(ReinforcementAgent):
           it will be called on your behalf
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        oldQValue = self.getQValue(state,action)
+        newQValue = reward + self.discount * self.computeValueFromQValues(nextState)
+
+        self.values[(state,action)] = (1 - self.alpha) * oldQValue +  self.alpha * newQValue
 
     def getPolicy(self, state):
         return self.computeActionFromQValues(state)
